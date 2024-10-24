cef8ef8f94a9aa447e00a103d69dbf1457f2bc56
IttaRodrigues <itamararodriguesdeoliveira@gmail.com>
2024-10-23 22:21:44 -0300

    Iniciando o Controllers , corrigindo erros

--- Services/UsuarioService.cs
+++ Services/UsuarioService.cs
@@ -11,48 +11,48 @@ using Sustem.Text.Encodings.Web;
 
 namespace OliCook.Services;
 
-    public class UsuarioService : IUsuarioService
+public class UsuarioService : IUsuarioService
+{
+
+    private readonly AddDbContext _contexto;
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    private readonly UserManager<IdentityUser> _userManager;
+
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IUserStore<IdentityUser> _userStore;
+    private readonly IUserEmailStore<IdentityUser> _emailStore;
+
+    private readonly IWebHostEnvironment _hostEnvironment;
+    private readonly IEmailSender _emailSender;
+    private readonly ILogger<UsuarioService> _logger;
+
+    public UsuarioService(
+       AddDbContext contexto,
+       SignInManager<IdentityUser> signInManager,
+       UserManager<IdentityUser> userManager,
+       IHttpContextAccessor httpContextAccessor,
+       IUserStore<IdentityUser> userStore,
+       IUserEmailStore<IdentityUser> emailStore,
+       IWebHostEnvironment hostEnvironment,
+       IEmailSender emailSender,
+       ILogger<UsuarioService> logger
+    )
+
     {
-        
-        private readonly AddDbContext _contexto;
-        private readonly SignInManager<IdentityUser> _signInManager;
-
-        private readonly UserManager<IdentityUser> _userManager;
-
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly  IUserStore<IdentityUser> _userStore;
-        private readonly IUserEmailStore<IdentityUser> _emailStore;
-
-        private readonly IWebHostEnvironment _hostEnvironment;
-        private readonly IEmailSender _emailSender;
-        private readonly ILogger<UsuarioService> _logger;
-
-         public UsuarioService(
-            AddDbContext contexto,
-            SignInManager<IdentityUser> signInManager,
-            UserManager<IdentityUser> userManager,
-            IHttpContextAccessor httpContextAccessor,
-            IUserStore<IdentityUser> userStore,
-            IUserEmailStore<IdentityUser> emailStore,
-            IWebHostEnvironment hostEnvironment,
-            IEmailSender emailSender,
-            ILogger<UsuarioService> logger
-         )
-
-         {
-            _contexto = contexto;
-            _signInManager = signInManager;
-            _userManager = userManager;
-            _httpContextAccessor = httpContextAccessor;
-            _userStore = userStore;
-            _emailStore = emailStore;
-            _hostEnvironment = hostEnvironment;
-            _emailSender = emailSender;
-            _logger = logger
+        _contexto = contexto;
+        _signInManager = signInManager;
+        _userManager = userManager;
+        _httpContextAccessor = httpContextAccessor;
+        _userStore = userStore;
+        _emailStore = emailStore;
+        _hostEnvironment = hostEnvironment;
+        _emailSender = emailSender;
+        _logger = logger
 
          }
-     public async Task<bool> ConfirmarEmail(string userId, string code)
-     {
+    public async Task<bool> ConfirmarEmail(string userId, string code)
+    {
         var user = await _userManager.FindByIdAsync(userId);
         if (user == null)
         {
@@ -61,9 +61,9 @@ namespace OliCook.Services;
         code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
         var result = await _userManager.ConfirmEmailAsync(user, code);
         return result.Succeeded;
-     } 
-     public async Task<UsuarioVM>GetUsuarioLogado()
-     {
+    }
+    public async Task<UsuarioVM> GetUsuarioLogado()
+    {
         var userId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimType.NameIdentifier);
         if (userId == null)
         {
@@ -71,29 +71,107 @@ namespace OliCook.Services;
         }
         var userAccount = await _userManager.FindByIdAsync(userId);
         var usuario = await _contexto.Usuarios.Where(uint => u.UsuarioId == userId).SingleOrdeDaultAsync();
-        var perfis - string.Join(", ", await _userManager.GetRolesAsync(userAccount));
+        var perfis -string.Join(", ", await _userManager.GetRolesAsync(userAccount));
         var admin = await _userManager.IsInRoleAsync(userAccount, "Administrador");
         UsuarioVM usuarioVM = new()
         {
-            Usuario
-        }
-     }
-
-
-
-
-
+            Usuario = userId,
+            Nome = ususario.Nome,
+            DataNascimento = usuario.DataNascimento,
+            Foto = usuario.Foto,
+            _emailSender = userAccount.Email,
+            UserName = userAccount.UserName,
+            Perfil = perfis,
+            IsAdmin = admin
+        };
+        return usuarioVM;
     }
 
+    public async Task<SignInResult> LoginUsuario(LoginVM login)
+    {
+        strting UserName = login.Email;
+        if (Helper.IsValidEmail(login.Email))
+        {
+            var user = awair _userManager.FindByIdAsync(login.Email);
+            if (user != null)
+                UserName = user.UserName;
+        }
 
+        var result = await _signInManager.PasswordSignInAsync(
+            UserName, login.Senha, login.Lembrar, lockoutOnFailure: true
+        );
+        if (result.Succeeded)
+            _logger.LogInformation($"Usuario {login.Email} acessou o sistema");
+        if (result.IsLockedOut)
+            _logger.LogWarning($"Usuario {login.Email} esta bloqueado");
 
+        return result;
+    }
 
+    public async Task LogoffUsuario()
+    {
+        _logger.LogInformation($"Usuario {ClaimTypes.Email} fez logoff");
+        await _signInManager.SignOutAsync();
+    }
 
+    public async Task<List<string>> RegistrarUsuario(RegistroVM registro)
 
+     var user = Activator.CreateInstance<IdentityUser>();
+
+    await _userStore.SetUserNameAsync(user, registro.Email, CancellationToken.None);
+    await _emailStore.SetEmailAsync(user, registro.Email, CancellationToken.None);
+    var result = await _userManager.CreateAsync(user, registro.Senha);
+
+     if (result.Succeeded)
+     {
+
+      _logger.LogInformation($"Novo usuario registrado com o email {user.Email}.");
+
+    var userId = await _userManager.GetUserIdAsync(user);
+    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+    code = WebEncoders.Base64UrlDecode(Encoding.UTF8.GetBytes(code));
+      var url = $"html://localhost:5143/Account/ConfirmarEmail?userId={userId}&code={code}";
+
+    await _userManager.AddToRoleAsync(user, "Usuario");
+    await _emailSender.SendEmailAsync(registro.Email, "OliCook - Criação de Conta", GetConfirmEmailHtml (HtmlEncoder.Default.Encode(url))); 
+
+      Usuario usuario = new()
+      {
+          UsuarioId = userId,
+          DataNascimento = registro.DataNascimento ?? DateTime.Now,
+          Nome = registro.Nome
+      };
+      if (registro.Foto != null)
+      {
+
+        string filenName = userId + Path.GeExtension(registro.Foto.FileName);
+    string uploads = Path.Combine(_hostEnvironment.WebRootPath, @"img\usuarios");
+    string newFile = Path.Combine(aploads, fileName);
+        using (var stream = new FileStream(newFile, FileMode.Create))
+        {
+            registro.Foto.CopyTo(stream);
+        }
+        usuario.Foto = @"img\usuarios\" + fileName;
+                    
+        }
+        _contexto.Add(usuario);
+await _contexto.SaveChangesAsync();
+
+return null;
+      }
+
+      List<string> errors = new();
+foreach (var error in result.Errors)
+{
+    errors.Add(TranslateIdentityErrors.TranslateErrorMessage(error.Code));
+}
+return errors;
+
+}  
 
     private string GetConfirmEmailHtml(string url)
-    {
-        var email = @"
+{
+    var email = @"
         <!DOCTYPE html>
         <html>
         <head>
@@ -349,7 +427,7 @@ namespace OliCook.Services;
         </body>
         </html>
         ";
-        return email;
-    }
+    return email;
+}
 
 
